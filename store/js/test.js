1453296af2c7d3ff36cb3b5e10a3d5bf72775956
MobiWise <96530435+mobiwise-codes@users.noreply.github.com>
2023-01-09 00:51:38 +0100

    Update test.js

--- store/js/test.js
+++ store/js/test.js
@@ -1,4 +1,4 @@
-export function test() {
+
   let xhr;
   document.getElementById("mwButton").addEventListener("click", sendXHR);
 
@@ -8,4 +8,4 @@ export function test() {
     xhr.open("GET", "http://127.0.0.1:5500/src/store.html", true);
     xhr.send();
   }
-}
+
