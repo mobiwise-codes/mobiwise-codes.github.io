c26ba4bc5783330a8d5b291a46c87a095736d515
MobiWise <96530435+mobiwise-codes@users.noreply.github.com>
2023-01-09 09:11:03 +0100

    Update test.js

--- store/js/test.js
+++ store/js/test.js
@@ -1,11 +1,13 @@
+alert('salut');
 
-  let xhr;
-  document.getElementById("mwButton").addEventListener("click", sendXHR);
 
-  function sendXHR() {
-    xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = mwContents;
-    xhr.open("GET", "http://127.0.0.1:5500/src/store.html", true);
-    xhr.send();
-  }
+//   let xhr;
+//   document.getElementById("mwButton").addEventListener("click", sendXHR);
+
+//   function sendXHR() {
+//     xhr = new XMLHttpRequest();
+//     xhr.onreadystatechange = mwContents;
+//     xhr.open("GET", "http://127.0.0.1:5500/src/store.html", true);
+//     xhr.send();
+//   }
 
