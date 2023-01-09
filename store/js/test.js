80cff7a4964110d358f3269b3dc1b4790bcb3965
MobiWise <96530435+mobiwise-codes@users.noreply.github.com>
2023-01-09 11:46:09 +0100

    Update test.js

--- store/js/test.js
+++ store/js/test.js
@@ -1,53 +1,6 @@
-mbrApp.alertDlg("Merci d'avoir installé MobiWise Store");
+mbrApp.alertDlg("TEST");
 
-/////////////// Height ///////////////
-function hauteur() {
-  const mwContainer = document.querySelector(".mwContainer");
-  mwContainer.style.height = `${window.innerHeight - 88}px`;
 
-  setTimeout(() => {
-    mwContainer.scrollTo(0, 0);
-  }, 500);
-}
-
-/////////////// Dark Mode ///////////////
-function addColor() {
-  const ifDarkMode = document.querySelector("body").classList.contains("dark-mode");
-  const darkStyle = document.createElement("style");
-  if (ifDarkMode) {
-    darkStyle.innerHTML = `.mwStore .modal-body{background-color:rgb(55,60,72)}.bgColor{background-color: rgb(37,41,49)}`;
-  } else {
-    darkStyle.innerHTML = `.mwStore .modal-body{background-color:rgb(247,247,247)}.bgColor{background-color: rgb(255,255,255)}`;
-  }
-  document.head.appendChild(darkStyle);
-}
-
-/////////////// SEARCH ///////////////
-function recherche() {
-  const searchField = document.getElementById("search");
-  searchField.addEventListener("input", searchList);
-  function searchList() {
-    const searchText = searchField.value.toLowerCase();
-    const listItems = document.querySelectorAll(".mwFilterItem");
-    const searchStyle = document.createElement("style");
-    listItems.forEach((item) => {
-      const itemTitle = item.textContent.toLowerCase();
-      if (itemTitle.includes(searchText)) {
-        item.classList.remove("out");
-        item.classList.add("in");
-        setTimeout(() => {
-          item.style.display = "block";
-        }, 300);
-      } else {
-        item.classList.remove("in");
-        item.classList.add("out");
-        setTimeout(() => {
-          item.style.display = "none";
-        }, 300);
-      }
-    });
-  }
-}
 
 
 
